c522d8a73ac6ffc6393472bacfe93ee5446a2fa9
qffset <34353933+qffset@users.noreply.github.com>
2018-02-12 22:28:13 +0300

    Add files via upload

---
+++ mysql.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+
+mysql_connect("localhost", "sms", "1111111") //параметры в скобках ("хост", "имя пользователя", "пароль")
+or die("<p>Ошибка подключения к базе данных! " . mysql_error() . "</p>");
+
+
+mysql_select_db("smsd2")//параметр в скобках ("имя базы, с которой соединяемся")
+ or die("<p>Ошибка выбора базы данных! ". mysql_error() . "</p>");
+
+$number = trim($_REQUEST['number']);
+$text = trim($_REQUEST['text']);
+//$reason = trim($_REQUEST['reason']);
+//$message = trim($_REQUEST['message']);
+
+$insert_sql = "INSERT INTO outbox (number, text)" .
+"VALUES('{$name}', '{$text}');";
+mysql_query($insert_sql);
+//$insert into outbox (number,text) values ('+79034405100','Проверка Proverka');
+
+
+
+echo "<p>Новая запись вставлена в базу!</p>";
+?>
